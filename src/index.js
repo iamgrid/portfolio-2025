39e4d3e0828d6fbd9721ee77550c692799f0c192
Andy Pataki <anpataki@gmail.com>
2025-08-27 14:40:21 +0200

    JS container object

--- src/index.js
+++ src/index.js
@@ -1,69 +1,74 @@
 import * as css from "./index.css";
 
 ("use strict");
-const HueChanger = {
-	rootEl: document.documentElement,
-	currentHue: 0,
-	cycle: function () {
-		this.currentHue = (this.currentHue + 0.5) % 360;
-		this.rootEl.style.setProperty("--main-hue", this.currentHue);
+const IAG = {
+	hueChanger: {
+		rootEl: document.documentElement,
+		currentHue: 0,
+		cycle: function () {
+			this.currentHue = (this.currentHue + 0.5) % 360;
+			this.rootEl.style.setProperty("--main-hue", this.currentHue);
+		},
+	},
+	milestones: {
+		VIEW_IDS: {
+			DEV: "DEV",
+			DESIGN: "DESIGN",
+		},
+		selectedView: "DEV",
+		selectView: function (viewId) {
+			const functionSignature = "index.js@milestones.selectView()";
+
+			console.log(functionSignature, {
+				viewId,
+				selectedView: this.selectedView,
+			});
+
+			if (viewId === this.selectedView) {
+				console.log(
+					functionSignature,
+					"No change in active view, returning early..."
+				);
+				return;
+			}
+			if (!(viewId in this.VIEW_IDS)) {
+				throw new Error(`Invalid viewId: ${viewId}`);
+			}
+
+			document
+				.getElementById(`milestones__${this.selectedView}`)
+				.classList.remove("milestones__view--active");
+			document.getElementById(
+				`milestones__${this.selectedView}-radio`
+			).ariaChecked = false;
+
+			document
+				.getElementById(`milestones__${viewId}`)
+				.classList.add("milestones__view--active");
+
+			document.getElementById(`milestones__${viewId}-radio`).ariaChecked = true;
+
+			this.selectedView = this.VIEW_IDS[viewId];
+		},
+	},
+	scrollToSection: function (sectionId) {
+		const functionSignature = "index.js@scrollToSection()";
+		console.log(functionSignature, { sectionId });
+
+		const sectionEl = document.getElementById(sectionId);
+		if (!sectionEl) {
+			throw new Error(`No section found with ID: ${sectionId}`);
+		}
+
+		sectionEl.scrollIntoView({ behavior: "smooth" });
+		window.history.pushState(null, "", `#${sectionId}`);
 	},
 };
 
 window.onload = () => {
 	setInterval(() => {
-		HueChanger.cycle();
+		IAG.hueChanger.cycle();
 	}, 50);
-};
 
-window.milestones = {
-	VIEW_IDS: {
-		DEV: "DEV",
-		DESIGN: "DESIGN",
-	},
-	selectedView: "DEV",
-	selectView: function (viewId) {
-		const functionSignature = "index.js@selectView()";
-
-		console.log(functionSignature, { viewId, selectedView: this.selectedView });
-
-		if (viewId === milestones.selectedView) {
-			console.log(
-				functionSignature,
-				"No change in active view, returning early..."
-			);
-			return;
-		}
-		if (!(viewId in milestones.VIEW_IDS)) {
-			throw new Error(`Invalid viewId: ${viewId}`);
-		}
-
-		document
-			.getElementById(`milestones__${milestones.selectedView}`)
-			.classList.remove("milestones__view--active");
-		document.getElementById(
-			`milestones__${milestones.selectedView}-radio`
-		).ariaChecked = false;
-
-		document
-			.getElementById(`milestones__${viewId}`)
-			.classList.add("milestones__view--active");
-
-		document.getElementById(`milestones__${viewId}-radio`).ariaChecked = true;
-
-		milestones.selectedView = milestones.VIEW_IDS[viewId];
-	},
-};
-
-window.scrollToSection = function (sectionId) {
-	const functionSignature = "index.js@scrollToSection()";
-	console.log(functionSignature, { sectionId });
-
-	const sectionEl = document.getElementById(sectionId);
-	if (!sectionEl) {
-		throw new Error(`No section found with ID: ${sectionId}`);
-	}
-
-	sectionEl.scrollIntoView({ behavior: "smooth" });
-	window.history.pushState(null, "", `#${sectionId}`);
+	window.IAG = IAG;
 };
